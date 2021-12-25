47b0b247b6be58f970b7d8c164460cf70a8f9a96
Phùng Học <phungdai.hoc@gmail.com>
2021-12-25 14:12:23 +0700

    update ảnh+ video

---
+++ demo2Adaboost.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +"haarcascade_frontalface_default.xml")
+
+video_capture = cv2.VideoCapture(1)
+
+while True:
+	ret, frame = video_capture.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+	faces =face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		cv2.rectangle(frame, (x,y),(x+w,y+h),(255,0,0),2)
+
+	cv2.imshow('video',frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+video_capture.release()
+cv2.destroyAllWindows()
